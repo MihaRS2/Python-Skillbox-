06bb696dbcb08707ffcafa9ededab9be54180132
Misha <ramishvilimihai4@gmail.com>
2023-01-30 10:25:12 +0300

    compelte number_module

---
+++ number_module/number_module.py
@@ -0,0 +1,9 @@
+#
+
+
+number = int(input("введите число x "))
+number_2 = -number
+if number >= 0:
+    print("вы ввели" " " + str(number) + " " "ответ" " " + str(number))
+else:
+    print("вы ввели" " " + str(number) + " " "ответ" " " + str(number_2))
