224f61087c8bc0b1822d69cfa9e40212ce00a792
Misha <ramishvilimihai4@gmail.com>
2023-02-02 15:54:41 +0300

    comlete

--- cycle/cycle.py
+++ cycle/cycle.py
@@ -1,19 +1,17 @@
 #Даша начала проходить циклы. Она написала программу, которая просто должна считать сумму чисел до тех пор, пока мы не ввели 0, но что-то пошло не так.
-#umber = int(input(‘Введите число: ’))
-#While number <= 0:
-# summ += number
-# number = int(input(‘Введите число: ’))
-#print(Summ)
 
 
-number_1 = int(input("Введите число 1 : "))
-number_2 = int(input("Введите число 2: "))
-summ = number_1 + number_2
-while number_1 != 0 or number_2 != 0:
-    print("сумма =" ,summ)
-    number_1 = int(input("Введите число 1 : "))
-    number_2 = int(input("Введите число 2: "))
-print("Вы ввели 0 программа завершилась")
+
+
+
+
+number = int(input('Введите число: '))
+summa = 0
+while number != 0:
+   summa += number
+   number = int(input('Введите число: '))
+print(summa)
+
 
  
  
